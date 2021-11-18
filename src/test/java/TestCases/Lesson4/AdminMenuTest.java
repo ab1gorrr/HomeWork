2bb31bc2141824009c298560953dd1467e481723
Ab1gorrr <mcvaps24@gmail.com>
2021-11-19 03:58:36 +0600

    Lesson4 Admin menu test fix

--- src/test/java/TestCases/Lesson4/AdminMenuTest.java
+++ src/test/java/TestCases/Lesson4/AdminMenuTest.java
@@ -4,9 +4,7 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.List;
@@ -19,7 +17,7 @@ public class AdminMenuTest {
     @Before
     public void start() {
         driver = new ChromeDriver();
-        driver.get(" http://localhost/litecart/admin/");
+        driver.get("http://localhost/litecart/admin/");
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
@@ -27,13 +25,22 @@ public class AdminMenuTest {
 
     @Test
     public void menuLinkTest(){
-       List<WebElement> element = driver.findElements(By.xpath("//*[@id=\"app-\"]"));
-      for(int i=0;i<element.size();i++){
-          element = driver.findElements(By.xpath("//*[@id=\"app-\"]"));
-          element.get(i).click();
-          Assert.assertFalse(driver.findElement(By.xpath("//*[@id=\"content\"]/h1")).getText().equals(null));
-      driver.navigate().back();
-      }
+        List<WebElement> mainOption = driver.findElements(By.cssSelector("ul#box-apps-menu>li"));
+        List<WebElement> sideOption;
+        for(int i=0;i<mainOption.size();i++){
+            mainOption = driver.findElements(By.cssSelector("ul#box-apps-menu>li"));
+            mainOption.get(i).click();
+            Assert.assertFalse(driver.findElement(By.xpath("//*[@id=\"content\"]/h1")).getText().equals(null));
+            sideOption = driver.findElements(By.cssSelector("ul.docs>li"));
+            if (sideOption !=null && sideOption.size() > 1){
+                for (int y=0;y<sideOption.size();y++){
+                    sideOption = driver.findElements(By.cssSelector("ul.docs>li"));
+                    sideOption.get(y).click();
+                    Assert.assertFalse(driver.findElement(By.xpath("//*[@id=\"content\"]/h1")).getText().equals(null));
+                }
+            }
+            driver.navigate().back();
+        }
     }
 
     @After
