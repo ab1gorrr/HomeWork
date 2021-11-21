4943ef941d762604a87cbd3e8c30765c9bab69c8
Ab1gorrr <mcvaps24@gmail.com>
2021-11-21 11:27:12 +0600

    Lesson5

--- src/test/java/TestCases/Lesson5/ProductCheck.java
+++ src/test/java/TestCases/Lesson5/ProductCheck.java
@@ -24,6 +24,8 @@ public class ProductCheck {
     @Test
     public void productVerify() {
         List<WebElement> productList = driver.findElements(By.cssSelector("div#box-campaigns ul li.product"));
+        double regularPrice;
+        double campaignPrice;
         if (productList.size()>0){
             String[] productLink = new String[productList.size()];
             String[] productName = new String[productList.size()];
@@ -34,10 +36,10 @@ public class ProductCheck {
                 productName[i] = productList.get(i).findElement(By.cssSelector("div.name")).getText();
                 productCostRegular[i] = productList.get(i).findElement(By.cssSelector("s.regular-price")).getText();
                 productCostCampaign[i] = productList.get(i).findElement(By.cssSelector("strong.campaign-price")).getText();
-                Assert.assertEquals(grayColorCheck(productList.get(i).findElement(By.cssSelector("s.regular-price")).getCssValue("color").replaceAll("rgba","")),true);
-                Assert.assertEquals(redColorCheck(productList.get(i).findElement(By.cssSelector("strong.campaign-price")).getCssValue("color").replaceAll("rgba","")),true);
-                double regularPrice = Double.parseDouble(productList.get(i).findElement(By.cssSelector("s.regular-price")).getCssValue("font-size").replaceAll("px",""));
-                double campaignPrice = Double.parseDouble(productList.get(i).findElement(By.cssSelector("strong.campaign-price")).getCssValue("font-size").replaceAll("px",""));
+                Assert.assertTrue(grayColorCheck(productList.get(i).findElement(By.cssSelector("s.regular-price")).getCssValue("color").replaceAll("rgba", "")));
+                Assert.assertTrue(redColorCheck(productList.get(i).findElement(By.cssSelector("strong.campaign-price")).getCssValue("color").replaceAll("rgba", "")));
+                regularPrice = Double.parseDouble(productList.get(i).findElement(By.cssSelector("s.regular-price")).getCssValue("font-size").replaceAll("px",""));
+                campaignPrice = Double.parseDouble(productList.get(i).findElement(By.cssSelector("strong.campaign-price")).getCssValue("font-size").replaceAll("px",""));
                 Assert.assertTrue(regularPrice<campaignPrice);
                 Assert.assertEquals(productList.get(i).findElement(By.cssSelector("s.regular-price")).getCssValue("text-decoration-line"),"line-through");
                 Assert.assertEquals(productList.get(i).findElement(By.cssSelector("strong.campaign-price")).getCssValue("font-weight"),"700");
@@ -48,6 +50,9 @@ public class ProductCheck {
                     Assert.assertEquals(productCostRegular[y],driver.findElement(By.cssSelector("s.regular-price")).getText().trim());
                     Assert.assertEquals(productName[y],driver.findElement(By.cssSelector("div#box-product h1")).getText());
                     Assert.assertEquals(productCostCampaign[y],driver.findElement(By.cssSelector("strong.campaign-price")).getText());
+                    regularPrice = Double.parseDouble(driver.findElement(By.cssSelector("s.regular-price")).getCssValue("font-size").replaceAll("px",""));
+                    campaignPrice = Double.parseDouble(driver.findElement(By.cssSelector("strong.campaign-price")).getCssValue("font-size").replaceAll("px",""));
+                    Assert.assertTrue(regularPrice<campaignPrice);
 
                 }
             }
@@ -57,7 +62,7 @@ public class ProductCheck {
 
     public List<Integer> colorToArray(String rgbaValue){
         Matcher m = Pattern.compile("\\d+").matcher(rgbaValue);
-        List<Integer> rgbaValueArray = new ArrayList<Integer>();
+        List<Integer> rgbaValueArray = new ArrayList<>();
         while(m.find()) {
             rgbaValueArray.add(Integer.parseInt(m.group()));
         }
@@ -68,11 +73,7 @@ public class ProductCheck {
         List<Integer> rgbaValueArray = colorToArray(rgbaValue);
         int gValue = rgbaValueArray.get(1);
         int bValue = rgbaValueArray.get(2);
-        if(gValue==bValue && bValue==0){
-            return true;
-        }else{
-            return false;
-        }
+        return gValue == bValue && bValue == 0;
     }
     public boolean grayColorCheck(String rgbaValue) {
         List<Integer> rgbaValueArray = colorToArray(rgbaValue);
@@ -80,11 +81,7 @@ public class ProductCheck {
         int rValue = rgbaValueArray.get(0);
         int gValue = rgbaValueArray.get(1);
         int bValue = rgbaValueArray.get(2);
-        if(rValue==gValue && gValue==bValue){
-            return true;
-        }else{
-            return false;
-        }
+        return rValue == gValue && gValue == bValue;
     }
 
     @After
