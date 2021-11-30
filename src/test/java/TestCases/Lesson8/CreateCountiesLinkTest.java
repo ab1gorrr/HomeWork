d8e442ac6b59b9614203697704496fe4780547a2
Ab1gorrr <mcvaps24@gmail.com>
2021-11-30 18:39:02 +0600

    Lesson8

---
+++ src/test/java/TestCases/Lesson8/CreateCountiesLinkTest.java
@@ -0,0 +1,51 @@
+package TestCases.Lesson8;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreateCountiesLinkTest {
+    WebDriver driver;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+    }
+
+    @Test
+    public void countyListAlphabet() {
+        driver.navigate().to("http://localhost/litecart/admin/?app=countries&doc=countries");
+        driver.findElement(By.cssSelector("td#content > div > a")).click();
+        String titleMainWindow = driver.getTitle();
+        String mainWindow = driver.getWindowHandle();
+        List<WebElement> linkList = driver.findElements(By.cssSelector("i.fa-external-link"));
+        for (WebElement webElement : linkList) {
+            webElement.click();
+            switchWindow(mainWindow);
+            Assert.assertTrue(titleMainWindow != driver.getTitle());
+            driver.close();
+            driver.switchTo().window(mainWindow);
+        }
+    }
+
+    public void switchWindow(String mainWindow){
+        ArrayList<String> newTab = new ArrayList<>(driver.getWindowHandles());
+        newTab.remove(mainWindow);
+        driver.switchTo().window(newTab.get(0));
+    }
+    @After
+    public void stop() {
+        driver.quit();
+    }
+}
